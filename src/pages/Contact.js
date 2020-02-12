bc33d04635e81019d71f99be802b45984c25d95c
betonit007 <timnagorski@hotmail.com>
2020-02-12 08:17:26 -0500

    working on contact

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -1,9 +1,82 @@
-import React from 'react'
+import React, { useState } from 'react'
+import './Contact.css'
+
 
 const ContactMe = () => {
+    const [formData, setFormData] = useState({
+        name: '',
+        email: '',
+        comments: ''
+    })
+
+    const onChange = e => setFormData({ ...formData, [e.target.name]: e.target.value })
+
+    const { email, name, comments } = formData;
+
+    const onSubmit = async e => {
+        e.preventDefault();
+        if (name && email) {
+          sendEmail();
+          setFormData({name: '', email: '', comments: ''})
+        }
+
+    }
+
+    const sendEmail = async () => {
+        let reply = await window.emailjs.send(
+            'gmail', 'portfolio',
+            formData
+        )
+
+        console.log(reply);
+    }
+
     return (
-        <div id="contact" className='bThree' style={{height:'100vh', backgroundColor:'red'}}>
-            Contact Me
+        <div id='contact' style={{ height: '100vh' }}>
+            <div className="contactContainer">
+                <h1 className="large text-primary">
+                    Contact Me
+            </h1>
+
+                <form className="form" onSubmit={onSubmit}>
+
+                    <div className="form-group">
+                        <input type="text"
+                            value={name}
+                            name='name'
+                            placeholder='Name'
+                            onChange={e => onChange(e)}
+                            required
+                        />
+                    </div>
+
+                    <div className="form-group">
+                        <input type="email"
+                            value={email}
+                            name='email'
+                            placeholder='Email'
+                            onChange={e => onChange(e)}
+                            required
+                        />
+                    </div>
+
+                    <div className="form-group">
+                        <textarea type="textbox"
+                            value={comments}
+                            name='comments'
+                            rows='5'
+                            placeholder='Comments'
+                            onChange={e => onChange(e)}
+                            required
+                        />
+                    </div>
+
+
+                    
+
+                    <input type="submit" value="Submit" className='btn' />
+                </form>
+            </div>
         </div>
     )
 }
